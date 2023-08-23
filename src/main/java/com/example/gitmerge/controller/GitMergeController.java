3be9829cc61426b42ca27681000f075ffccd779a
danicodes2023 <136983509+danicodes2023@users.noreply.github.com>
2023-08-23 12:08:16 +0530

    Update GitMergeController.java

--- src/main/java/com/example/gitmerge/controller/GitMergeController.java
+++ src/main/java/com/example/gitmerge/controller/GitMergeController.java
@@ -20,5 +20,30 @@ public class GitMergeController {
     public String gitMerge(){
         return gitMergeService.gitMerge();
     }
+    /*
+    @PostMapping("testgitpost")
+    //public String testPostGit(){
+    public ResponseEntity<String> handleWebhook(@RequestBody Map<String, Object> payload) {
+            // Access the "name" field from the payload
+
+            Map<String, Object> repository = (Map<String, Object>) payload.get("repository");
+            if(repository != null) {
+                String repositoryName = (String) repository.get("name");
+                if(repositoryName != null) {
+                    System.out.println("Received payload from repository: " + repositoryName);
+                    String responseMessage = "Received payload from repository: " + repositoryName;
+                    return ResponseEntity.ok(responseMessage);
+                }
+                else{
+                    return ResponseEntity.badRequest().body("Repository name missing");
+                }
+            }
+            else{
+                return ResponseEntity.badRequest().body("Repository missing");
+            }
+
+    }
+    
+    */
 
 }
